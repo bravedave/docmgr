9dbe21302979a70fe5304ed58013db3c37e6a9f6
Dave Bray <david@brayworth.com.au>
2020-06-01 14:32:02 +1000

    Update launcher.php

--- src/app/launcher.php
+++ src/app/launcher.php
@@ -10,33 +10,17 @@
 
 namespace dvc\docmgr;
 
-if ( class_exists('application')) {
-	class launcher extends \application {
-		function __construct( $rootPath) {
-			$this->defaultController = 'dvc\docmgr\controller';
-			parent::__construct( $rootPath);
+class launcher extends \application {
+	function __construct( $rootPath) {
+		$this->defaultController = 'dvc\docmgr\controller';
+		parent::__construct( $rootPath);
 
-		}
+	}
 
-		static function run( $dir = null) {
-			new self( $dir ? $dir : dirname( __DIR__));
-
-		}
+	static function run( $dir = null) {
+		new self( $dir ? $dir : dirname( __DIR__));
 
 	}
 
 }
-else {
-	class launcher {
-		static function run() {
-			/**
-			 * Yeah - the Minimum Viable Product
-			 */
-			header( 'Content-Type: text/plain');
-			print \file_get_contents( __DIR__ . '/readme.txt');
 
-		}
-
-	}
-
-}
